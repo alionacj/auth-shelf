0e3a2fb2fd9b957f0f4e44dcfed1e63ea91effc0
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-14 13:36:55 -0600

    est. reducers and sagas

--- src/components/ShelfPage/ShelfPage.jsx
+++ src/components/ShelfPage/ShelfPage.jsx
@@ -2,22 +2,25 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ShelfItem from '../ShelfItem/ShelfItem';
 import ShelfForm from '../ShelfForm/ShelfForm';
+import { useSelector } from 'react-redux';
 
 
 function ShelfPage() {
 
-  let dummyData = [{
-    id: 1,
-    description: 'Boy, this is a nice boat.',
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Motorboat_at_Kankaria_lake.JPG/1200px-Motorboat_at_Kankaria_lake.JPG',
-    username: 'mark'
-  },
-  {
-    id: 2,
-    description: 'Boy, this is a nice boat again.',
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Motorboat_at_Kankaria_lake.JPG/1200px-Motorboat_at_Kankaria_lake.JPG',
-    username: 'James'
-  }]
+  const shelfItems = useSelector(store => store.shelf)
+
+  // let dummyData = [{
+  //   id: 1,
+  //   description: 'Boy, this is a nice boat.',
+  //   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Motorboat_at_Kankaria_lake.JPG/1200px-Motorboat_at_Kankaria_lake.JPG',
+  //   username: 'mark'
+  // },
+  // {
+  //   id: 2,
+  //   description: 'Boy, this is a nice boat again.',
+  //   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Motorboat_at_Kankaria_lake.JPG/1200px-Motorboat_at_Kankaria_lake.JPG',
+  //   username: 'James'
+  // }]
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -28,7 +31,7 @@ function ShelfPage() {
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      {dummyData.map((item) => {
+      {shelfItems.map((item) => {
           return <ShelfItem item={item} key={item.id}/>
       })}
 
